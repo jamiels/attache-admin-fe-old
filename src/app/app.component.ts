0c025b662d49632ca2ea90600bea51b586d37e78
radekxrandom <jozwa.zawadiaka@gmail.com>
2021-02-01 22:43:44 +0100

    sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { SimplywhiteComponent } from "./@pages/layouts/simplywhite/simplywhite.component";
+import { RootLayout } from "./@pages/layouts/root/root.component";
 
 @Component({
   selector: "app-root",
-  template: "<router-outlet></router-outlet>",
-  styleUrls: ["./app.component.scss"]
+  templateUrl: "./app.component.html",
+  styleUrls: ["./@pages/layouts/simplywhite/simplywhite.component.scss"],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
-  title = "app works!";
+export class AppComponent extends RootLayout implements OnInit {
+  title = "App works";
+  menuLinks = [
+    {
+      label: "Users",
+      routerLink: "users",
+      iconType: "fi",
+      iconName: "users"
+    },
+    {
+      label: "Videos",
+      routerLink: "videos",
+      iconType: "fi",
+      iconName: "list"
+    },
+    {
+      label: "Quote servers",
+      routerLink: "quoteservers",
+      iconType: "fi",
+      iconName: "cpu"
+    }
+  ];
+  ngOnInit() {
+    this.changeLayout("menu-pin");
+    //Will sidebar close on screens below 1024
+    this.autoHideMenuPin();
+  }
 }
